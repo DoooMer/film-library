195df977e0b9018e19af565c4eab8d160862e34f
Lev Leyman <lev.leiman@yandex.ru>
2017-01-05 13:47:39 +0400

    add main view

---
+++ Filmlibrary/views/Main.py
@@ -0,0 +1,34 @@
+from PyQt5.QtWidgets import QDesktopWidget
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QWidget
+
+
+class Main(QWidget):
+    width = 600
+    height = 400
+    title = "Film library 4.1"
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.resize(self.width, self.height)
+        self.center()
+        self.setWindowTitle(self.title)
+        self.show()
+
+    def center(self):
+        frame_geometry = self.frameGeometry()
+        center = QDesktopWidget().availableGeometry().center()
+        frame_geometry.moveCenter(center)
+        self.move(frame_geometry.topLeft())
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message', "Are you sure to quit?", QMessageBox.Yes | QMessageBox.No,
+                                     QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
