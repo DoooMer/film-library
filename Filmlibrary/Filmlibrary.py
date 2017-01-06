86d8be66e2e30b29d354983bce950af10f955b2b
Lev Leyman <lev.leiman@yandex.ru>
2017-01-07 00:07:22 +0400

    add creation a new database file

--- Filmlibrary/Filmlibrary.py
+++ Filmlibrary/Filmlibrary.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QApplication
+from peewee import SqliteDatabase
 
 from Filmlibrary.ui.Main import Main
 
@@ -9,5 +10,18 @@ class Filmlibrary:
         super().__init__()
 
     def run(self):
-        main = Main()
+        main = Main(app=self)
+        self.dbFile = "default.db"
         return self.qApp.exec_()
+
+    def connect(self, file=None):
+        if file is not None:
+            self.dbFile = file
+
+        self.close()
+        self.db = SqliteDatabase(self.dbFile)
+        self.db.connect()
+
+    def close(self):
+        if hasattr(self, 'db') and isinstance(self.db, SqliteDatabase):
+            self.db.close()
--- Filmlibrary/ui/Main.py
+++ Filmlibrary/ui/Main.py
@@ -1,4 +1,6 @@
+from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtWidgets import QDesktopWidget
+from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QMessageBox
 
@@ -10,9 +12,13 @@ class Main(QMainWindow, Ui_MainWindow):
     height = 400
     title = "Film library 4.1"
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, app=None):
         super(Main, self).__init__(parent)
         self.setupUi(self)
+        self.app = app
+        self.actionNew.triggered.connect(self.createFileDialog)
+        self.actionOpen.triggered.connect(self.openFileDialog)
+        self.actionExit.triggered.connect(self.close)
 
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
@@ -32,6 +38,17 @@ class Main(QMainWindow, Ui_MainWindow):
                                      QMessageBox.No)
 
         if reply == QMessageBox.Yes:
+            self.app.close()
             event.accept()
         else:
             event.ignore()
+
+    def openFileDialog(self):
+        dbfile = QFileDialog.getOpenFileName(self, 'Open file', '', "DB files (*.db)")
+        self.app.dbFile = dbfile[0]
+        self.app.connect()
+
+    def createFileDialog(self):
+        dbfile = QFileDialog.getSaveFileName(self, 'Create file', 'default.db', '*.db')
+        self.app.dbFile = dbfile[0]
+        self.app.connect()
